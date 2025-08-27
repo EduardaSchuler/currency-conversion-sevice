966e50d49e2d88aa243b6294e883ef5f855ca219
MARIA SCHULER <m.schuler@edu.pucrs.br>
2025-08-27 20:07:06 -0300

    estrutura base

---
+++ src/main/java/com/engsoft2/currecy_conversion_service/CurrecyConversionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.engsoft2.currecy_conversion_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrecyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrecyConversionServiceApplication.class, args);
+	}
+
+}
